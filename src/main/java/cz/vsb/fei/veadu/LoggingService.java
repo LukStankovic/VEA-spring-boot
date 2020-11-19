11729c143e82e2400d6bbb0c269b78600800ab6e
Lukáš Stankovič <lukasstankovic@gmail.com>
2020-11-20 00:40:47 +0100

    aop

--- src/main/java/cz/vsb/fei/veadu/LoggingService.java
+++ src/main/java/cz/vsb/fei/veadu/LoggingService.java
@@ -1,34 +1,34 @@
 package cz.vsb.fei.veadu;
 
 import java.util.Arrays;
-import java.util.stream.Collectors;
+import java.util.List;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Component
 @Aspect
 public class LoggingService {
-
 	
-	@Before("execution(* cz.vsb.fei.vealab01.controlers.*.*(..))")
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
+	
+	@Before("execution(* cz.vsb.fei.veadu.controlers.*.*(..))")
 	public void log(JoinPoint joinPoint) {
-		System.out.println(joinPoint.getSignature());
-		System.out.println(
-				Arrays.asList(joinPoint.getArgs()).stream().
-				map(a -> a.toString()).collect(Collectors.joining(", ")));
-	}
+		log.info("Loading controller: " + joinPoint.getSignature());	
+		
+		List<Object> arguments = Arrays.asList(joinPoint.getArgs());
 
-	@Around("execution(* cz.vsb.fei.vealab01.controlers.*.*(..))")
-	public Object monitor(ProceedingJoinPoint pjp) throws Throwable{
-		System.out.println("monitoring....");
-		Object retVal = pjp.proceed(pjp.getArgs());
-		System.out.println("....done");
-		return retVal;
+		if (joinPoint.getArgs().length > 1) {
+			log.info("Post arguments: ");
+			for (Object a : arguments) {
+				log.info(a.toString());
+			}
+		}
+		log.info("End of action.");
 	}
-
 }
